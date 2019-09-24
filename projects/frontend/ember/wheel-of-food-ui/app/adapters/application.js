4be947f0db73b5b6e1ad6ad59f2bd8a6e42d1b1c
CodeDestroyer666 <kkeronen@gmail.com>
2019-09-24 23:04:49 +0300

    Ember tutorial done

---
+++ projects/frontend/ember/wheel-of-food-ui/app/adapters/application.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+// import ENV from 'wheel-of-food-ui/config/environment';
+
+export default DS.JSONAPIAdapter.extend({
+    // host: ENV.APP.API_URL,
+    namespace: 'api'
+});
