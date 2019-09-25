350d3fb2be98580636015f189171ef5cdab99a8c
CodeDestroyer666 <kkeronen@gmail.com>
2019-09-25 10:04:50 +0300

    Disabled compression with aws-serverless-express for now

--- projects/backend/Node/wheel-of-food-api/src/index.js
+++ projects/backend/Node/wheel-of-food-api/src/index.js
@@ -13,7 +13,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors());
 }
 
-app.use(compression());
 app.use('/api/v1', api);
 
 const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
@@ -27,6 +26,9 @@ if (isInLambda) {
         serverlessExpress.proxy(server, event, context);
     }
 } else {
+    // Cannot use compression with aws-serverless-express. Causes ERR_CONTENT_DECODING_FAILED
+    // TODO: Fix solution later
+    app.use(compression());
     module.exports = app;
 }
 
