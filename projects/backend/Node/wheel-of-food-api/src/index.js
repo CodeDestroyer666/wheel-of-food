a4d1e87d523ab2ebac8dccef7df6c95d2e9c0b59
CodeDestroyer666 <kkeronen@gmail.com>
2019-10-21 21:42:04 +0300

    Added missing attribute for Recipe data model

--- projects/backend/Node/wheel-of-food-api/src/index.js
+++ projects/backend/Node/wheel-of-food-api/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 
-const api = require('./api');
+const api = require('./api').default;
 const corsOptions = require('./cors');
 
 
