1560f9a68dc92b45de279528fab03759608256a3
CodeDestroyer666 <kkeronen@gmail.com>
2019-10-21 21:14:10 +0300

    Added  some server side data models

---
+++ projects/backend/Node/wheel-of-food-api/src/ORM/models/index.js
@@ -0,0 +1,92 @@
+export class Rental {
+    constructor() {
+        this.type = 'rentals';
+        this.id = '';
+        this.attributes = {
+            title: '',
+            owner: '',
+            city: '',
+            category: '',
+            bedrooms: 1,
+            image: '',
+            description: ''
+        };
+    }
+}
+
+export class Menu {
+    constructor() {
+        this.type = 'menus';
+        this.id = '';
+        this.attributes = {
+            createdTime: Date,
+            recipes: [],
+        };
+    }
+}
+
+export class Recipe {
+    constructor() {
+        this.type = 'recipes';
+        this.id = '';
+        this.attributes = {
+            name: '',
+            instructions: '',
+            ingredients: [],
+            tags: [],
+            createdTime: Date,
+        };
+    }
+}
+
+export class Ingredient {
+    constructor() {
+        this.type = 'ingredients';
+        this.id = '';
+        this.attributes = {
+            name: ''
+        };
+    }
+}
+
+export class IngredientAmount extends Ingredient {
+    constructor() {
+        super();
+        super.attributes.amount = '';
+    }
+}
+
+export class Tag {
+    constructor() {
+        this.type = 'tags';
+        this.id = '';
+        this.attributes = {
+            name: ''
+        };
+    }
+}
+
+export class RecipeEvent {
+    constructor() {
+        this.type = 'recipeevents';
+        this.attributes = {
+            name: '',
+            recipeId: '',
+            createdTime: Date,
+        };
+    }
+}
+
+export class RecipeIncludeEvent extends RecipeEvent {
+    constructor() {
+        super();
+        super.attributes.name = 'INCLUDE';
+    }
+}
+
+export class RecipeExcludeEvent extends RecipeEvent {
+    constructor() {
+        super();
+        super.attributes.name = 'EXCLUDE';
+    }
+}
